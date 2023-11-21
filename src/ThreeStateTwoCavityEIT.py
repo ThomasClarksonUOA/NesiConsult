db93f14b9559aa9c16dc7f92e1903b3fdf3e9900
Thomas Clarkson <tcla272@UoA.auckland.ac.nz>
2023-11-22 09:55:53 +1300

    Refactored code to use configparser Updated the config file and refactored ThreeStateTwoCavityEIT.py to use configparser. Now loads in parameters from the config file. Main Hamiltonian builder updated to accept this format. Data is now saved in a 5xN array.

--- src/ThreeStateTwoCavityEIT.py
+++ src/ThreeStateTwoCavityEIT.py
@@ -5,31 +5,35 @@ Created on Wed Jul 19 16:19:58 2023
 @author: tcla272
 """
 
+#%% Imports
 from qutip import *
 import numpy as np
 import matplotlib.pyplot as plt
+import configparser
+import os
 
+#%% Hamiltonian Builder
 def ThreeStateEIT(params):
-    da = params['da']
-    dea = params['dea']
-    db = params['db']
-    deb = params['deb']
+    da = float(params['da'])*2*np.pi
+    dea = float(params['dea'])*2*np.pi
+    db = float(params['db'])*2*np.pi
+    deb = float(params['deb'])*2*np.pi
 
-    ga = params['ga']
-    gb = params['gb']
+    ga = float(params['ga'])*2*np.pi
+    gb = float(params['gb'])*2*np.pi
 
-    ea = 0.5*params['ea']
-    eb = 0.5*params['eb']
+    ea = 0.5*float(params['ea'])*2*np.pi
+    eb = 0.5*float(params['eb'])*2*np.pi
     
-    kappa = params['kappa']
-    kappb = params['kappb']
+    kappa = float(params['kappa'])*2*np.pi
+    kappb = float(params['kappb'])*2*np.pi
 
-    gamma = params['gamma']
-    G = params['G']
-    O = params['O']
+    gamma = float(params['gamma'])*2*np.pi
+    G = float(params['G'])*2*np.pi
+    O = float(params['O'])*2*np.pi
     
-    Ma = params['Ma']
-    Mb = params['Mb']
+    Ma = int(params['Ma'])
+    Mb = int(params['Mb'])
     
     a = tensor(destroy(Ma), qeye(Mb), qeye(3))
     b = tensor(qeye(Ma), destroy(Mb), qeye(3))
@@ -79,8 +83,8 @@ def ThreeStateEIT(params):
     c_ops_list.append(np.sqrt(2*kappa)*a)
     c_ops_list.append(np.sqrt(2*kappb)*b)
     c_ops_list.append(np.sqrt(G)*tensor(qeye(Ma),qeye(Mb),g1*g2.dag()))
-    # c_ops_list.append(np.sqrt(gamma)*tensor(qeye(Ma),qeye(Mb),g2*e0.dag()))
-    # c_ops_list.append(np.sqrt(gamma)*tensor(qeye(Ma),qeye(Mb),g1*e0.dag()))
+    c_ops_list.append(np.sqrt(gamma)*tensor(qeye(Ma),qeye(Mb),g2*e0.dag()))
+    c_ops_list.append(np.sqrt(gamma)*tensor(qeye(Ma),qeye(Mb),g1*e0.dag()))
     
     # Output cavity annihilation operators and atomic population operators
     operators = [a,b]
@@ -88,82 +92,80 @@ def ThreeStateEIT(params):
     operators.append(pop2g)
     operators.append(pop0e)
     
+    # Return Hamiltonian, Collapse Operators and System Operators
     return H, c_ops_list, operators
 
-
-probe = np.linspace(-15,15,151)
-transmission = np.zeros(len(probe))
-reflection = np.zeros(len(probe))
-states = np.zeros((3,len(probe)))
-
-opts = Options()
-opts.store_final_state = True
-opts.store_states = False
-opts.nsteps = 10000
-
-for idx, freq in enumerate(probe):
-    params = {
-        'da' : 0*2*np.pi,
-        'dea' : freq*2*np.pi,
-        'db' : 0*2*np.pi,
-        'deb' : 0*2*np.pi,
-    
-        'ga' : 8*2*np.pi,
-        'gb' : 8*2*np.pi, #6.8 donald
-    
-        'ea' : 2*2*np.pi,
-        'eb' : 0*2*np.pi,
-    
-        'kappa' : 1*2*np.pi, #15*2*np.pi, #5.1  
-        'kappb' : 1*2*np.pi, #15*2*np.pi, #3.4  
-            
-        'gamma' : 0.1*2*np.pi, #5.234
-        'G' : 0.5*2*np.pi,
-        'O' : 0*2*np.pi,
-            
-        # Number of atoms and cavity basis. M>=3 for photon statistics
-        'N' : 1,
-        'Ma' : 8,
-        'Mb' : 8,
-        }
-    
+#%% Define Single Parameter Solve
+def steadyStateExpect(params):
+    # Generate Hamiltonian
     H, c_ops_list, operators = ThreeStateEIT(params)
+    
+    # Solve for Steady State
     rhoss = steadystate(H,c_ops_list,method = 'iterative-lgmres',use_precond = True)
     
     a = operators[0]
     b = operators[1]
     
-    states[:,idx] = np.real(expect(operators[2:],rhoss))
+    states = np.real(expect(operators[2:],rhoss))
+    modea = np.real(expect(a.dag()*a,rhoss))
+    modeb = np.real(expect(b.dag()*b,rhoss))
     
-    x = expect(a,rhoss)
-    y = params['kappa']*x/(params['ea'])
-    z = y-1
-    
-    transmission[idx] = np.real(expect(a.dag()*a,rhoss))#np.real(y*y.conjugate()) #
-    reflection[idx] = np.real(z*z.conjugate()) #np.real(expect(b.dag()*b,rhoss))#
-    print(f'\r{idx}', end=' ',flush=True)
-    
-#%%    
-fontsize = 30
-plt.rcParams.update({'font.size': fontsize})
-    
-fig,ax = plt.subplot_mosaic("AABB;AACC;DDEE;DD..", figsize=(60,60))
-ax["A"].plot(probe,transmission,label = 'a')
-ax["D"].plot(probe,reflection,label = 'b')
-    
-ax["B"].plot(probe,states[0,:],label = '1')
-ax["C"].plot(probe,states[1,:],label = '2')
-ax["E"].plot(probe,states[2,:],label = '3')
-        
-        
-for axs in ax:
-    ax[axs].set_xlabel(r'$\Delta_a$')
-    ax[axs].set_ylabel('Population')
-    ax[axs].legend()
-        
-ea = np.round(params["ea"],0)
-eb = np.round(params["eb"],0)
-plt.savefig(f'ThreeLevelEIT_{ea}_{eb}.svg',format='svg',bbox_inches='tight')
-    
+    return np.array([modea,modeb,*states])
 
+#%% Main
+if __name__ == "__main__":
+    # Read in parameters from config file
+    configs = configparser.ConfigParser()
+    path = 'C:\\Users\\tcla272\\Python\\nonclassical_light_nesi\\configs\\default.ini'
+    configs.read(path)
+    params = configs['DEFAULT']
+    
+    # Set Solver Options
+    opts = Options()
+    opts.store_final_state = True
+    opts.store_states = False
+    opts.nsteps = 10000
+    
+    # Build iterable
+    iterstart = float(params['iterstart'])
+    iterend = float(params['iterend'])
+    iternum = int(params['iternum'])
+    probe = np.linspace(iterstart,iterend,iternum)
+    
+    # Run loop
+    for idx, freq in enumerate(probe):
+        # Update Parameters
+        params[params['key']] = str(freq)
+        # Run Solver
+        result = steadyStateExpect(params)
+        # Save Results to Array
+        if idx == 0:
+            output = result
+        else:
+            output = np.vstack((output,result))
+        # Progress Bar
+        print(f'\r{idx}', end=' ',flush=True)
+        
+#%% Plotting
+    # Update Plot Parameters
+    fontsize = 30
+    plt.rcParams.update({'font.size': fontsize})
+        
+    fig,ax = plt.subplot_mosaic("AAABBB;AAABBB;AAACCC;DDDCCC;DDDEEE;DDDEEE", figsize=(60,60))
+    ax["A"].plot(probe,output[:,0],label = 'a')
+    ax["D"].plot(probe,output[:,1],label = 'b')
+        
+    ax["B"].plot(probe,output[:,2],label = '1')
+    ax["C"].plot(probe,output[:,3],label = '2')
+    ax["E"].plot(probe,output[:,4],label = '3')
+    
+    for axs in ax:
+        ax[axs].set_xlabel(r'$\Delta_a$')
+        ax[axs].set_ylabel('Population')
+        ax[axs].legend()
+            
+    ea = np.round(float(params['ea']),0)
+    eb = np.round(float(params['eb']),0)
 
+#%% Saving Results
+    plt.savefig(f'ThreeLevelEIT_{ea}_{eb}.svg',format='svg',bbox_inches='tight')
